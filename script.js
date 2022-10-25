42d2f120a4b32781d946db4f2b720c6c901e92c5
Mozib <mozib.diu@gmail.com>
2022-10-25 12:31:15 +0600

    ball shoot done

--- script.js
+++ script.js
@@ -1,21 +1,118 @@
+shoot = false;
+dragged = false;
+minDis = 15;
+overhead = 60;
+missY = -175;
+goalY = -135;
+ballSize = .35;
+shootDuration = .2;
+bounceBaseY = -85;
+
 Draggable.create('.pp_ball_replica', {
-  type: "xy", edgeResistance: .99,cursor: 'auto', bounds: {
-      top: 100, left: 50, width: 200, height: 135,
-  }, 
-  // onDragEnd: function () {
-  //     if (hitStatus) {
-  //         gsap.to(".element_4", { display: "block", opacity: 1, duration: .5 });
-  //         gsap.to(".element_6", { duration:.8, x: 0, y: 0});
-  //     }
-  // }, 
-  // onDrag: function () {
-  //     if (this.hitTest(".element_5", 55) && hitStatus) {
-  //         hitStatus = false;
-  //         console.log('hitted');
-  //         hitHair();
-  //     }
-  // },
-  // onDragStart: function () {
-  //     gsap.to(".element_4", { display: "none", opacity: 0, duration: .5 });
-  // }
+  type: "x,y", edgeResistance: .99, cursor: 'auto', bounds: {
+    top: 100, left: 50, width: 200, height: 135,
+  },
+  onDragEnd: function () {
+    py = Math.abs(this.y);
+    if (py <= minDis) {
+      gsap.to(".pp_ball_replica", { duration: .1, x: 0, y: 0 });
+    } else if (py < overhead && (!shoot)) {
+      shoot = true;
+      console.log('shooting...end');
+      shootBall(this.x, py);
+    }
+    console.log(this.x, this.y);
+    gsap.to(".pp_ball_replica", { duration: .1, x: 0, y: 0 });
+
+
+  },
+
+  onDrag: function () {
+    console.log(this.pointerX);
+    py = Math.abs(this.y);
+    if (py > minDis && py < overhead && (!dragged)) {
+      dragged = true;
+      dragInterval = setInterval(() => {
+        if (!shoot) {
+          console.log('shooting...ondrag');
+          shootBall(this.x, py);
+
+          shoot = true;
+        }
+      }, 800);
+    }
+    // up: for drag shot
+
+    if (py > overhead && (!shoot)) {
+      shoot = true;
+      console.log('shooting...max');
+      shootBall(this.x, py);
+
+    }
+  }
 })
+
+
+
+function shootBall(checkX, checkY) {
+  lastX = checkX * 1.722;
+  clearInterval(dragInterval);
+
+
+  if (checkY > minDis && checkY < overhead) {
+    lastY = goalY;
+  } else {
+    lastY = missY;
+  }
+
+  console.log(`shot to ${checkX} ${checkY}`);
+  console.log(`shot to ${lastX} ${lastY}`);
+
+  gsap.to(".pp_ball", {
+    y: lastY, x: lastX, scaleX: ballSize, scaleY: ballSize, duration: shootDuration, ease: Linear.easeNone,
+    onComplete: function () {
+      console.log(lastX, lastY);
+      bounceBall(lastX, lastY);
+      // clearBall();
+    }
+  });
+}
+
+
+function clearBall() {
+  setTimeout(() => {
+    gsap.to(".pp_ball", { opacity: 1, y: 0, x: 0, scaleX: 1, scaleY: 1, duration: .001 });
+    shoot = false;
+    dragged = false;
+  }, 600);
+}
+
+
+
+
+
+function bounceBall(posX, posY) {
+  console.log('baseLine:', posX, posY);
+  difference = posY - bounceBaseY;
+  
+  if (posX > 25) {
+    moveX = [30, 50, 70, 90, 110];
+  } else if (posX < -25) {
+    moveX = [-30, -50, -70, -90, -110];
+  } else if (posX < 0) {
+    moveX = [-20, -30, -40, -50, -60];
+  } else if (posX >= 0) {
+    moveX = [20, 30, 40, 50, 60];
+  }
+
+  gsap.timeline({ defaults: { ease: Linear.easeNone }, onComplete: clearBall })
+    .to(".pp_ball", { delay: .01, opacity: 1, y: bounceBaseY, x: posX + moveX[0], duration: .2 })
+
+    .to(".pp_ball", { opacity: 1, y: bounceBaseY + (difference / 1.5), x: posX + moveX[1], duration: .15 }, ">")
+
+    .to(".pp_ball", { opacity: .8, y: bounceBaseY, x: posX + moveX[2], duration: .15 }, ">")
+
+    .to(".pp_ball", { opacity: .5, y: bounceBaseY + (difference / 3), x: posX + moveX[3], duration: .15 }, ">")
+
+    .to(".pp_ball", { opacity: 0.2, y: bounceBaseY, x: posX + moveX[4], duration: .15 }, ">");
+}
